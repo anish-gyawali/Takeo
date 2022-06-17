30c70805cf02ae1c75ad7bb1d22260880b870e96
Anish Gyawali <anish.gyawali7@gmail.com>
2022-06-16 21:49:39 -0500

    start of react hooks

--- WeekFive/reactapp/src/App.js
+++ WeekFive/reactapp/src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+
 function App() {
+  console.log("i am from line 4")
+  const [state, setState]=React.useState(0);
+  console.log("i am from line 6");
+
+  function handleclick(){
+    console.log("i am from handdle click function ")
+    setState(state+1);
+  }
   return (
     <div className="App">
-     hello 
+     <button onClick={handleclick}>{state}</button>
     </div>
   );
 }
