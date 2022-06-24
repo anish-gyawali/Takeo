213e804d938d045808bdd55488ce7545daed64c4
Anish Gyawali <anish.gyawali7@gmail.com>
2022-06-23 21:03:23 -0500

    Thursday: worked with form to handle input and rating for feedback form also added Select and Rating reusable component

--- WeekSix/reactapp/src/App.jsx
+++ WeekSix/reactapp/src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import FeedBackForm from "./components/FeedBackForm";
 import FeedBackList from "./components/FeedBackList";
 import Header from "./components/Header";
 import data from "./data/feedback";
 
 function App() {
+  const [text, setText] = useState("");
+  const [feedback, setFeedback] = useState(data);
+
+  const feedBackData = (message) => {
+    setText(message);
+    console.log(message);
+  };
+
   return (
     <>
-      <div>
-        <Header text={"Header"} />
-        <FeedBackForm />
-        <FeedBackList feedback={data} />
-      </div>
+      <Header text={"Header"} />
+      <FeedBackForm fbData={feedBackData} />
+      <FeedBackList feedback={data} />
     </>
   );
 }
--- WeekSix/reactapp/src/components/FeedBackForm.jsx
+++ WeekSix/reactapp/src/components/FeedBackForm.jsx
@@ -1,15 +1,51 @@
 import { useState } from "react";
+import Rating from "./Rating";
+import Button from "./shared/Button";
+import Card from "./shared/Card";
 
-function FeedBackForm() {
-  const [feedback, setFeedback] = useState("");
-  function handleChange(e) {
-    setFeedback(e.target.value);
-    console.log(e.target.value);
-  }
+function FeedBackForm({ fbData }) {
+  const [feedbackText, setFeedbackText] = useState("");
+  const [message, setMessage] = useState("");
+  const [disable, setDisable] = useState(true);
+  const [rating, setRating] = useState(true);
+
+  const handleChange = (e) => {
+    if (e.target.value.trim().length < 10) {
+      setDisable(true);
+      setMessage("Text length should be grater than 10 character");
+    } else {
+      setDisable(false);
+      setMessage("");
+      setFeedbackText(e.target.value);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(rating);
+    fbData(feedbackText);
+  };
+
   return (
-    <form>
-      <input type="text" onChange={handleChange} />
-    </form>
+    <Card>
+      <div>Select Rating</div>
+      <Rating select={(rating) => setRating(rating)} />
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="write your feedback here"
+          />
+        </div>
+        <Button version="secondary" type="submit" isDisabled={disable}>
+          Send
+        </Button>
+        {/* <div>{message}</div>
+        <div>{feedbackText}</div> */}
+
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
   );
 }
 
