7dbf9025dd409f756205dc88614effffe0346f67
Anish Gyawali <anish.gyawali7@gmail.com>
2022-06-29 20:50:30 -0500

    implemented session delete function by using filter

--- WeekSeven/reactapp/src/App.js
+++ WeekSeven/reactapp/src/App.js
@@ -6,23 +6,24 @@ import Header from "./components/Header";
 import data from "./data/feedback";
 
 function App() {
-  const [text, setText] = useState("");
+  const [, setText] = useState("");
   const [feedback, setFeedback] = useState(data);
-  
 
   const feedBackData = (message) => {
     setText(message);
   };
 
   const deleteFeedback = (id) => {
-    console.log(id);
+    if (window.confirm("Delete  feedback")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
   };
 
   return (
     <>
       <Header text={"Header"} />
       <FeedBackForm fbData={feedBackData} />
-      <FeedBackList feedback={data} deleteFeedback={deleteFeedback} />
+      <FeedBackList feedback={feedback} deleteFeedback={deleteFeedback} />
       <FeedBackStats data={feedback} />
     </>
   );
