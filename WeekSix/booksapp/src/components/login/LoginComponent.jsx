db301a8a03ba3280493f50fd0ae9f17f92d82759
Anish Gyawali <anish.gyawali7@gmail.com>
2022-06-29 22:28:14 -0500

    added login component

--- WeekSix/booksapp/src/components/login/LoginComponent.jsx
+++ WeekSix/booksapp/src/components/login/LoginComponent.jsx
@@ -1,37 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Button from "../common/Button";
 import Input from "../common/Input";
 
 function LoginComponent() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
   const navigate = useNavigate();
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const login = () => {
+    navigate("/books");
+  };
   return (
     <>
-      <h1>LoginComponent</h1>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
+      <div className="loginContainer">
+        <h1>LoginComponent</h1>
+
         <Input
           type={"text"}
           label={"Username"}
           placeholder={"Input username here"}
+          value={username}
+          onChange={handleUsername}
         />
         <Input
           label={"Password"}
           type={"password"}
           placeholder={"Input Password here"}
+          value={password}
+          onChange={handlePassword}
         />
-        <Button
-          label={"Login"}
-          onClick={() => {
-            navigate("/books");
-          }}
-        />
+        <Button label={"Login"} onClick={login} />
       </div>
     </>
   );
