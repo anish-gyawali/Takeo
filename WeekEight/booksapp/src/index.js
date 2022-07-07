fb71d214138742bc80383bb7d683547ad6859548
Anish Gyawali <anish.gyawali7@gmail.com>
2022-07-06 19:46:56 -0500

    initial redux setup

--- WeekEight/booksapp/src/index.js
+++ WeekEight/booksapp/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
+import configureStore from "./store";
 import UserContextProvider from "./components/context/UserContext";
 
+const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <UserContextProvider>
-      <App />
-    </UserContextProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </BrowserRouter>
+  </Provider>
 );
