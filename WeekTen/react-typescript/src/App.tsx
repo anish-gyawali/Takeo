960b44a691a91ac5a1b59e6cafe0a224b8516dc2
Anish Gyawali <anish.gyawali7@gmail.com>
2022-07-17 19:37:40 -0500

    typed children in react

--- WeekTen/react-typescript/src/App.tsx
+++ WeekTen/react-typescript/src/App.tsx
@@ -1,8 +1,22 @@
+import React from "react";
 
+type NameTagChildren={children:React.ReactNode};
+
+//for multiple element React.ReactNode
+//for one element JSX.Element
+
+//function NameTag({children}:{children:React.ReactNode};
+
+function NameTag({children}:NameTagChildren){
+  return <>{children}</>
+}
 function App() {
   return (
-    <div className="App">
-      I am here
+    <div >
+      <NameTag>
+        <div>Hello</div>
+        <div>I love react</div>
+      </NameTag>
     </div>
   );
 }
