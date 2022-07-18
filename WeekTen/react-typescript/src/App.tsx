bfe261b171db84e2e6afbb651f146d4a465ddee1
Anish Gyawali <anish.gyawali7@gmail.com>
2022-07-17 20:40:18 -0500

    use of useState hooks and scss

--- WeekTen/react-typescript/src/App.tsx
+++ WeekTen/react-typescript/src/App.tsx
@@ -1,24 +1,41 @@
-import React from "react";
+import { useEffect, useState } from 'react';
+import { questions } from './questions';
 
-type NameTagChildren={children:React.ReactNode};
+type QuestionProps = {
+  question: string;
+  answer: string;
+};
 
-//for multiple element React.ReactNode
-//for one element JSX.Element
+const Question = ({ question, answer }: QuestionProps) => {
+  const [toggle,setToggle]=useState<boolean|null>(null)
 
-//function NameTag({children}:{children:React.ReactNode};
-
-function NameTag({children}:NameTagChildren){
-  return <>{children}</>
-}
-function App() {
+  useEffect(() => { 
+    setTimeout(()=>{
+      setToggle(true)
+    },2000)
+  }, [])
+  
   return (
-    <div >
-      <NameTag>
-        <div>Hello</div>
-        <div>I love react</div>
-      </NameTag>
-    </div>
+    <article className="question">
+      <header>{question}</header>
+      <p className="answer">
+        <span className={`${toggle? 'blurred':''}`}>{answer}</span>
+      </p>
+      <footer>
+        <button onClick={()=>setToggle(!toggle)}>Toggle Answer</button>
+      </footer>
+    </article>
   );
-}
+};
+
+const App = () => {
+  return (
+    <main>
+      {questions.map((q) => (
+        <Question question={q.question} answer={q.answer} key={q.id} />
+      ))}
+    </main>
+  );
+};
 
 export default App;
