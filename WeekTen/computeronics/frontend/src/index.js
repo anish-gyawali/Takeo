2c6a3ffc46669e1cdff8e3ac5d6cce68f7b28a20
Anish Gyawali <anish.gyawali7@gmail.com>
2022-07-24 14:04:39 -0500

    initial setup for react redux

--- WeekTen/computeronics/frontend/src/index.js
+++ WeekTen/computeronics/frontend/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
+import configureStore from "./store";
 
+const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
