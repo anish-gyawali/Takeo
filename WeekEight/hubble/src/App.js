b1cb278e2840f20734acb580b202d1a9e7340525
Anish Gyawali <anish.gyawali7@gmail.com>
2022-07-28 19:42:03 -0500

    final commit

--- WeekEight/hubble/src/App.js
+++ WeekEight/hubble/src/App.js
@@ -1,5 +1,28 @@
+import { ThemeProvider } from "styled-components";
+import { Container } from "./components/styles/Container.styled";
+import { theme } from "./theme";
+
+import GlobalStyles from "./components/styles/Global";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
+import content from "./content";
+
 function App() {
-  return <div>start here</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
