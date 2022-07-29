f844acbc20cd5429c692478b7aa06ffdc7a7e115
Anish Gyawali <anish.gyawali7@gmail.com>
2022-07-28 20:37:05 -0500

    Task 5 completed

--- WeekSix/assignment/src/App.js
+++ WeekSix/assignment/src/App.js
@@ -4,6 +4,7 @@ import * as React from "react";
 import Task2 from "./Task2";
 import Task3 from "./Task3";
 import Task4 from "./Task4";
+import Task5 from "./Task5";
 
 function countReducer(state, newState) {
   return newState;
@@ -28,7 +29,8 @@ function App() {
       {/* <Counter /> */}
       {/* <Task2 /> */}
       {/* <Task3 /> */}
-      <Task4 />
+      {/* <Task4 /> */}
+      <Task5 />
     </>
   );
 }
