48d3ccff793d4f2869abed84058a92d34f929265
Anish Gyawali <anish.gyawali7@gmail.com>
2022-08-01 22:32:37 -0500

    created test to check if button is clicked or not

--- FinalWeek/test-react-app/src/HelloWorld.js
+++ FinalWeek/test-react-app/src/HelloWorld.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const HelloWorld = () => {
+  const [showTestingComp, setShowTestingComp] = useState(false);
+
+  const showPara = () => {
+    setShowTestingComp(true);
+  };
   return (
     <>
       <h2>Hello world</h2>
-      <p>Testing the component</p>
+      {showTestingComp && <p>Testing the component</p>}
+      {!showTestingComp && <p>Testing the component not showing</p>}
+      <button onClick={showPara}>Show Paragraph</button>
     </>
   );
 };
--- FinalWeek/test-react-app/src/HelloWorld.test.js
+++ FinalWeek/test-react-app/src/HelloWorld.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvents from "@testing-library/user-event";
 import HelloWorld from "./HelloWorld";
 
 describe("Testing text of component", () => {
@@ -7,11 +8,47 @@ describe("Testing text of component", () => {
     const helloWorldElem = screen.getByText("Hello world", { exact: false });
     expect(helloWorldElem).toBeInTheDocument();
   });
-  test("renders 'Testing the component'", () => {
+
+  test("renders 'Testing the component not showing'", () => {
     render(<HelloWorld />);
-    const testingCompElem = screen.getByText("Testing the component", {
-      exact: false,
+    const NotestingCompElem = screen.getByText(
+      "Testing the component not showing",
+      {
+        exact: true,
+      }
+    );
+    expect(NotestingCompElem).toBeInTheDocument();
+  });
+
+  test("renders 'Testing the component' should not be there", () => {
+    render(<HelloWorld />);
+    const testingCompElem = screen.queryByText("Testing the component", {
+      exact: true,
+    });
+    //expect(testingCompElem).toBeNull();
+    expect(testingCompElem).not.toBeInTheDocument();
+  });
+
+  test("renders 'Testing the component' when button Clicked", () => {
+    render(<HelloWorld />);
+    const showParaBtn = screen.getByRole("button");
+    userEvents.click(showParaBtn);
+    const testingCompElem = screen.queryByText("Testing the component", {
+      exact: true,
     });
     expect(testingCompElem).toBeInTheDocument();
+  });
+
+  test("renders 'Testing the component not showing' when button Clicked", () => {
+    render(<HelloWorld />);
+    const showParaBtn = screen.getByRole("button");
+    userEvents.click(showParaBtn);
+    const NoTestingCompElem = screen.queryByText(
+      "Testing the component not showing",
+      {
+        exact: true,
+      }
+    );
+    expect(NoTestingCompElem).toBeNull();
   });
 });
